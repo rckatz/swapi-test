7da6c249e87bfd0a48ad61c87b0071f83d6ed22b
Sam Poe <rckatz@hotmail.com>
2019-11-22 23:21:19 -0500

    Updates

---
+++ precache-manifest.e340476b08b0ac4c93aa9bd4976bea18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdb02037d5caaa19e329ba5a745fe13a",
+    "url": "/swapi-test/index.html"
+  },
+  {
+    "revision": "f1668ae9055cadf73a3a",
+    "url": "/swapi-test/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "689ed5d3fd2c80ca2a08",
+    "url": "/swapi-test/static/css/main.1a80503a.chunk.css"
+  },
+  {
+    "revision": "f1668ae9055cadf73a3a",
+    "url": "/swapi-test/static/js/2.ca5adc2f.chunk.js"
+  },
+  {
+    "revision": "689ed5d3fd2c80ca2a08",
+    "url": "/swapi-test/static/js/main.0a8d95e4.chunk.js"
+  },
+  {
+    "revision": "f86affa2ece34d3cfebd",
+    "url": "/swapi-test/static/js/runtime-main.8accbae9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/swapi-test/static/media/SEGA.12a3db8b.woff"
+  }
+]);
